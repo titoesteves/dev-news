ed641b866debccb432a95512fafdcddfd429c6cc
Tito Esteves <norbertotito_esteves@comcast.com>
2016-07-22 21:05:55 -0700

    add handlebars engine

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+var handlebars = require('express-handlebars');
 var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
 
+app.engine('html', handlebars({extname: 'html', defaultLayout: 'main' }));
+app.set('view engine', 'html');
+
 app.get('/', function(req, res){
-  res.sendFile('./index.html');
+  res.render('index', { text: 'handlebars' });
 });
 
 app.listen(3000, function(){

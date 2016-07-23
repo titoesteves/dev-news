bbadf1a239b4fe58624b6032ec15cbf6ba319d6e
Tito Esteves <norbertotito_esteves@comcast.com>
2016-07-23 12:06:27 -0700

    create sites array and pass to handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var sites = [{site: 'Hacker News'},{site: 'Echo JS'},{site: 'Slashdot'},
+  {site: 'Product Hunt'}, {site: 'Github Trend'}];
 router.get('/', function(req, res){
-  res.render('index', { text: 'handlebars!!' });
+  res.render('index', { sites: sites });
 });
 
 module.exports = router;

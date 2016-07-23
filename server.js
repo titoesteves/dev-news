6c4bcd7562a627847cdd8aea19f0774a2e2469ad
Tito Esteves <norbertotito_esteves@comcast.com>
2016-07-22 21:30:43 -0700

    add body parser

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var handlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
@@ -6,6 +7,9 @@ app.use(express.static('public'));
 
 app.engine('html', handlebars({extname: 'html', defaultLayout: 'main' }));
 app.set('view engine', 'html');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
   res.render('index', { text: 'handlebars' });

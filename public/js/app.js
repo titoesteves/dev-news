5d6daa6fcdcbd7972b80edd37bae9aa6d2a70541
Tito Esteves <norbertotito_esteves@comcast.com>
2016-07-23 17:29:32 -0700

    set up default display on page load

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
-  console.log('hello world');
+  
+
+  var $site = $('.aside li'), $links = $('.links');
+  defaultDisplay($links);
+
+
+  $site.on('click', function(event){
+    console.log('clicked');
+    // $.ajax({});
+  });
 });
+
+function defaultDisplay($links){
+  $.get('https://news.ycombinator.com/rss', function(data){
+    var $xml = $(data);
+    function getEachItem(index){
+      if( index < 20 ) {
+        var $this = $(this),
+          obj = {
+            title: $this.find('title').text(),
+            link: $this.find('link').text(),
+            description: $this.find('description').text(),
+            pubDate: $this.find('pubDate').text(),
+            author: $this.find('author').text()
+          };
+        var $link = $('<li class="link"/>');
+        var $title = $(`<a class="title" href="${obj.link}"/>`);
+        $title.text(obj.title).appendTo($link);
+        $links.prepend($link);
+      }
+    }
+    $xml.find('item').each(getEachItem);
+  });
+}

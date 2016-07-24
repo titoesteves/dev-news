cd1f532b7aaab1c4b19639be1a21d4d2fc050572
Tito Esteves <norbertotito_esteves@comcast.com>
2016-07-23 19:42:09 -0700

    refactor to make it more modular

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,39 @@
-$(document).ready(function(){
-  
+var $links = $('.links'),
+  $site = $('.aside li');
 
-  var $site = $('.aside li'), $links = $('.links');
-  defaultDisplay($links);
+$(document).ready(function() {
+  defaultDisplay();
 
-
-  $site.on('click', function(event){
+  $site.on('click', function() {
     console.log('clicked');
     // $.ajax({});
   });
 });
 
-function defaultDisplay($links){
-  $.get('https://news.ycombinator.com/rss', function(data){
+function createLink(obj) {
+  var $link = $('<li class="link"/>');
+  var $title = $(`<a class="title" href="${obj.link}" target="_blank"/>`);
+  $title.text(obj.title).appendTo($link);
+  $links.prepend($link);
+}
+
+function getEachItem(index) {
+  if (index < 20) {
+    var $this = $(this),
+      obj = {
+        title: $this.find('title').text(),
+        link: $this.find('link').text(),
+        description: $this.find('description').text(),
+        pubDate: $this.find('pubDate').text(),
+        author: $this.find('author').text()
+      };
+    createLink(obj);
+  }
+}
+
+function defaultDisplay() {
+  $.get('https://news.ycombinator.com/rss', function(data) {
     var $xml = $(data);
-    function getEachItem(index){
-      if( index < 20 ) {
-        var $this = $(this),
-          obj = {
-            title: $this.find('title').text(),
-            link: $this.find('link').text(),
-            description: $this.find('description').text(),
-            pubDate: $this.find('pubDate').text(),
-            author: $this.find('author').text()
-          };
-        var $link = $('<li class="link"/>');
-        var $title = $(`<a class="title" href="${obj.link}"/>`);
-        $title.text(obj.title).appendTo($link);
-        $links.prepend($link);
-      }
-    }
     $xml.find('item').each(getEachItem);
   });
 }

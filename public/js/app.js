244a0d4719fe2f52e567053cc20f3edcaa9dd68e
Tito Esteves <norbertotito_esteves@comcast.com>
2016-07-24 19:13:06 -0700

    remove unused functions since refactoring

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,13 @@ var $links = $('.links'),
   $site = $('.aside li');
 
 $(document).ready(function() {
-
   $site.on('click', siteClick);
 });
 
-function siteClick(event){
+function siteClick(event) {
   var siteUrl = '/' + event.target.innerHTML;
-  if(siteUrl.indexOf('Echo') > -1){
-    $.get(siteUrl, function(data){
+  if (siteUrl.indexOf('Echo') > -1) {
+    $.get(siteUrl, function(data) {
       data.forEach(getEachJsonItem);
     });
   } else {
@@ -27,46 +26,10 @@ function displayLink(obj) {
   $links.prepend($link);
 }
 
-function getEachRssItem(index) {
-  if (index < 20) {
-    var $this = $(this),
-      obj = {
-        title: $this.find('title').text(),
-        link: $this.find('link').text()
-      };
-    displayLink(obj);
-  }
-}
-
-function getEachJsonItem(obj){
-  var link = { link: obj.link, title: obj.title };
+function getEachJsonItem(obj) {
+  var link = {
+    link: obj.link,
+    title: obj.title
+  };
   displayLink(link);
 }
-
-function getLinks(url) {
-  $links.html('');
-  var type = '';
-  if(url.indexOf('rss') > -1){
-    type = 'rss';
-    getSite(type, url);
-  } else {
-    $.get(url, function(data){
-      console.log('data', data);
-      getSite(null, url);
-    });
-  }
-}
-
-function getSite(type, siteUrl){
-  if(type === 'rss'){
-    $.get(siteUrl, function(data) {
-      var $xml = $(data);
-      $xml.find('item').each(getEachRssItem);
-    });
-  } else {
-    $.get(siteUrl, function(data) {
-      data.forEach(getEachJsonItem);
-    });
-  }
-}
-
